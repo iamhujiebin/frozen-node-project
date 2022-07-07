247e1ca47b7c422d9fe85b29c35d4905bd4859e8
JiebinHu <458249864@qq.com>
2022-07-07 20:27:06 +0800

    feat:加上屏幕录像

--- frozen/FrontProject/src/controller/main.js
+++ frozen/FrontProject/src/controller/main.js
@@ -11,19 +11,37 @@ const Main = Vue.component("main", {
     },
 
     mounted() {
-        this._socket = io();
-        this._remoteStream = new MediaStream();
-        this.$refs.remote_preview.srcObject = this._remoteStream;
-        this.$refs.chat_list.setIO(this._socket);
-        this.addSocketListeners();
+        this.initRemoteStream();
+        this.initDisplayStream();
+        this.initSocketIO();
     },
 
     methods: {
+        initSocketIO() {
+            this._socket = io();
+            this.$refs.chat_list.setIO(this._socket);
+            this.addSocketListeners();
+        },
         addSocketListeners() {
             // this._socket.on("msg", e => {
             //     console.log(e)
             // })
-        }
+        },
+        async initRemoteStream() {
+            // this._remoteStream = new MediaStream();
+            this._remoteStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+            this.$refs.remote_preview.srcObject = this._remoteStream;
+        },
+        async initDisplayStream() {
+            this._displayStream = await navigator.mediaDevices.getDisplayMedia();
+            this.$refs.display_preview.srcObject = this._displayStream;
+            this._recorder = new MediaRecorder(this._displayStream, { mimeType: "video/webm;codesc=h264" });
+            this._recorder.ondataavailable = this.recorder_dataAvailableHandler.bind(this);
+        },
+        recorder_dataAvailableHandler(e) {
+            console.log(e);
+            this.currentWebmData = e.data;
+        },
     }
 })
 
