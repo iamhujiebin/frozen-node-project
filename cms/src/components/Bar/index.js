9f69d136f6a28fe51946c161729c8cd265b62209
JiebinHu <458249864@qq.com>
2022-08-02 00:17:12 +0800

    echart的简单倒入

---
+++ cms/src/components/Bar/index.js
@@ -0,0 +1,36 @@
+import * as echarts from 'echarts'
+import { useEffect, useRef } from 'react'
+
+function echartInit (node, xData, sData, title) {
+  const myChart = echarts.init(node)
+  myChart.setOption({
+    title: {
+      text: title,
+    },
+    tooltip: {},
+    xAxis: {
+      data: xData
+    },
+    yAxis: {},
+    series: [
+      {
+        name: '人数',
+        type: 'bar',
+        data: sData
+      }
+    ]
+  })
+}
+
+function Bar ({ style, xData, sData, title }) {
+  const nodeRef = useRef(null)
+  useEffect(() => {
+    echartInit(nodeRef.current, xData, sData, title)
+  }, [xData, sData, title])
+
+  return (
+    <div ref={nodeRef} style={style}></div>
+  )
+}
+
+export default Bar
--- cms/src/pages/Home/index.js
+++ cms/src/pages/Home/index.js
@@ -1,6 +1,23 @@
+import Bar from "@/components/Bar"
+import "@/index.scss"
 
 const Home = () => {
-  return <div>home</div>
+  return (
+    <div className="home">
+      <Bar
+        style={{ width: '500px', height: '400px' }}
+        xData={['vue', 'angular', 'react']}
+        sData={[50, 40, 90]}
+        title='三大框架满意度'
+      />
+      <Bar
+        style={{ width: '500px', height: '400px' }}
+        xData={['vue', 'angular', 'react']}
+        sData={[80, 20, 100]}
+        title='三大框架使用度'
+      />
+    </div>
+  )
 }
 
 export default Home
