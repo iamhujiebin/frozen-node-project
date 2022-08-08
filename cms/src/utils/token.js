92db807ca65b220902cd04b4045d63c462ffd5ab
JiebinHu <458249864@qq.com>
2022-08-08 18:48:48 +0800

    feat：更新key名字

--- cms/src/utils/token.js
+++ cms/src/utils/token.js
@@ -1,11 +1,11 @@
-const TOKEN_KEY = 'FROZEN'
-const USRR_KEY = 'USER'
+const TOKEN_KEY = 'TOKEN'
+const USER_KEY = 'USER'
 
 const getToken = () => localStorage.getItem(TOKEN_KEY)
 const setToken = (token) => localStorage.setItem(TOKEN_KEY, token)
 const clearToken = () => localStorage.removeItem(TOKEN_KEY)
 
-const setUser = (user) => localStorage.setItem(USRR_KEY, user)
-const getUser = () => localStorage.getItem(USRR_KEY)
+const setUser = (user) => localStorage.setItem(USER_KEY, user)
+const getUser = () => localStorage.getItem(USER_KEY)
 
 export { getToken, setToken, clearToken, setUser, getUser }
