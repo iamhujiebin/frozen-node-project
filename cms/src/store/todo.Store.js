20ecde85a2d7a17a6460ff2d965d0b85872bb801
JiebinHu <458249864@qq.com>
2023-05-05 21:48:10 +0800

    feat:开始有搞头了！明天弄两个服务端接口机型了

--- cms/src/store/todo.Store.js
+++ cms/src/store/todo.Store.js
@@ -1,43 +1,63 @@
-import { makeAutoObservable } from "mobx"
+import {makeAutoObservable} from "mobx"
 
 class TodoStore {
-  list = [
-    {
-      id: 1,
-      htmlId: "todo-1",
-      name: '学习react',
-      isDone: false,
-    },
-    {
-      id: 2,
-      htmlId: "todo-2",
-      name: '搞掂mobx',
-      isDone: false,
+    list = [
+        {
+            id: 1,
+            htmlId: "todo-1",
+            name: '学习react',
+            isDone: false,
+        },
+        {
+            id: 2,
+            htmlId: "todo-2",
+            name: '搞掂mobx',
+            isDone: false,
+        }
+    ]
+
+    constructor() {
+        // 监听这个类的属性变化
+        makeAutoObservable(this)
     }
-  ]
-  constructor() {
-    // 监听这个类的属性变化
-    makeAutoObservable(this)
-  }
-  singleCheck = (id, checked) => {
-    let item = this.list.find(item => item.id === id)
-    item.isDone = checked
-  }
-  allCheck = (checked) => {
-    this.list.forEach(item => item.isDone = checked)
-  }
-  delTodo = (id) => {
-    this.list = this.list.filter(item => item.id !== id)
-  }
-  addTodo = (newTodo) => {
-    this.list.push(newTodo)
-  }
-  get isFinished () {
-    return this.list.filter(item => item.isDone).length
-  }
-  get isAll () {
-    return this.list.every(item => item.isDone)
-  }
+
+    getMyTodo = () => {
+        this.list = [
+            {
+                id: 3,
+                htmlId: "todo-3",
+                name: '重温react',
+                isDone: false,
+            },
+            {
+                id: 4,
+                htmlId: "todo-4",
+                name: '搞掂todolist',
+                isDone: false,
+            }
+        ]
+    }
+    singleCheck = (id, checked) => {
+        let item = this.list.find(item => item.id === id)
+        item.isDone = checked
+    }
+    allCheck = (checked) => {
+        this.list.forEach(item => item.isDone = checked)
+    }
+    delTodo = (id) => {
+        this.list = this.list.filter(item => item.id !== id)
+    }
+    addTodo = (newTodo) => {
+        this.list.push(newTodo)
+    }
+
+    get isFinished() {
+        return this.list.filter(item => item.isDone).length
+    }
+
+    get isAll() {
+        return this.list.every(item => item.isDone)
+    }
 }
 
 export default TodoStore
