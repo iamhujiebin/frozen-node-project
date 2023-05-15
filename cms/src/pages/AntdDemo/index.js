0e951d31859e39348a3e34655332903a3733b790
hujiebin <iamhujiebin@users.noreply.github.com>
2023-05-15 16:38:41 +0800

    feat:可拖拽的ui、

--- cms/src/pages/AntdDemo/index.js
+++ cms/src/pages/AntdDemo/index.js
@@ -1,7 +1,15 @@
-import {Radio, Space, Button, Tooltip, Divider, Dropdown, Avatar, List, Skeleton, Input, Form} from "antd";
+import {Radio, Space, Button, Tooltip, Divider, Dropdown, Avatar, List, Skeleton, Input, Form, Table} from "antd";
 import {BrowserView, MobileView, isBrowser, isMobile} from 'react-device-detect'
 import {SearchOutlined} from "@ant-design/icons"
 import {useEffect, useState} from "react";
+import {DndContext} from '@dnd-kit/core';
+import {
+    arrayMove,
+    SortableContext,
+    useSortable,
+    verticalListSortingStrategy,
+} from '@dnd-kit/sortable';
+import {CSS} from '@dnd-kit/utilities';
 
 const AntdDemo = () => {
     const btnClick = () => {
@@ -46,6 +54,73 @@ const AntdDemo = () => {
                 // window.dispatchEvent(new Event('resize'));
             });
     };
+    const [dataSource, setDataSource] = useState([
+        {
+            key: '1',
+            name: 'John Brown',
+            age: 32,
+            address:
+                'Long text Long text Long text Long text Long text Long text Long text Long text Long text Long text Long text Long text Long text Long text Long text Long text Long text Long text Long text Long text Long text Long text Long text Long text Long text Long text Long text Long text Long text Long text Long text Long text',
+        },
+        {
+            key: '2',
+            name: 'Jim Green',
+            age: 42,
+            address: 'London No. 1 Lake Park',
+        },
+        {
+            key: '3',
+            name: 'Joe Black',
+            age: 32,
+            address: 'Sidney No. 1 Lake Park',
+        },
+    ]);
+    const columns = [
+        {
+            title: 'Name',
+            dataIndex: 'name',
+        },
+        {
+            title: 'Age',
+            dataIndex: 'age',
+        },
+        {
+            title: 'Address',
+            dataIndex: 'address',
+        },
+    ];
+    const Row = (props) => {
+        const {attributes, listeners, setNodeRef, transform, transition, isDragging} = useSortable({
+            id: props['data-row-key'],
+        });
+        const style = {
+            ...props.style,
+            transform: CSS.Transform.toString(
+                transform && {
+                    ...transform,
+                    scaleY: 1,
+                },
+            ),
+            transition,
+            cursor: 'move',
+            ...(isDragging
+                ? {
+                    position: 'relative',
+                    zIndex: 9999,
+                }
+                : {}),
+        };
+        return <tr {...props} ref={setNodeRef} style={style} {...attributes} {...listeners}/>
+    }
+    const onDragEnd = ({active, over}) => {
+        if (active.id !== over?.id) {
+            setDataSource((prev) => {
+                const activeIndex = prev.findIndex((i) => i.key === active.id)
+                const overIndex = prev.findIndex((i) => i.key === over?.id)
+                return arrayMove(prev, activeIndex, overIndex)
+            })
+        }
+    }
     if (isMobile) {
         return <div> This content is available only on mobile</div>
     }
@@ -98,6 +173,25 @@ const AntdDemo = () => {
                     <Button type={'primary'} htmlType={'submit'}>Summit</Button>
                 </Form.Item>
             </Form>
+            <Divider orientation={'left'}>Divider</Divider>
+            <DndContext onDragEnd={onDragEnd}>
+                <SortableContext
+                    // rowKey array
+                    items={dataSource.map((i) => i.key)}
+                    strategy={verticalListSortingStrategy}
+                >
+                    <Table
+                        components={{
+                            body: {
+                                row: Row,
+                            },
+                        }}
+                        rowKey="key"
+                        columns={columns}
+                        dataSource={dataSource}
+                    />
+                </SortableContext>
+            </DndContext>
         </>
     )
 }
