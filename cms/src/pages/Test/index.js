72353a0268da478a0c4baf647bcb6bf138fd765b
hujiebin <iamhujiebin@users.noreply.github.com>
2023-06-27 18:31:17 +0800

    feat:vap

--- cms/src/pages/Test/index.js
+++ cms/src/pages/Test/index.js
@@ -1,61 +1,77 @@
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import {getToken} from "@/utils";
 import {createWebSocket} from "@/components/WebSocket";
 import {PubSub} from 'pubsub-js';
 import ReactPlayer from "react-player";
+import Vap from 'video-animation-player'
+import config from './demo.json'
+import demo from './demo.mp4'
+import config2 from './vapc.json'
+import demo2 from './video.mp4'
+import man from "@/assets/man.png"
+import woman from "@/assets/woman.png"
+
 
 function Test() {
-    const [message, setMessage] = useState("todo")
+    let vapPlayer = useRef(null);
+    let vapPlayer2 = useRef(null);
+
     useEffect(() => {
-        //订阅 'message' 发布的发布的消息
-        let messageSocket = PubSub.subscribe('message', function (topic, message) {
-            //message 为接收到的消息
-            setMessage(message)
-        })
-        //卸载组件 取消订阅
-        return () => {
-            PubSub.unsubscribe(messageSocket);
-        }
-    })
-    useEffect(() => {
-        const access_token = getToken()
-        const url = `${process.env.REACT_APP_WS_URL}/${access_token}`
-        createWebSocket(url)
-    }, [])
-    const handleReady = () => {
-        console.log("视频准备就绪");
-    };
-
-    const handleStart = () => {
-        console.log("视频开始播放");
-    };
-
-    const handlePause = () => {
-        console.log("视频暂停");
-    };
-
-    const handleEnded = () => {
-        console.log("视频播放结束");
-    };
-    return (<div className='App'>
-        <div className='container'>
-            {message}
-            <div>
-                <ReactPlayer
-                    // url="https://www.youtube.com/watch?v=dM_zrOuN1FY"
-                    url="https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_20mb.mp4"
-                    playing={true}
-                    loop={true}
-                    controls={true}
-                    volume={0.5}
-                    onReady={handleReady}
-                    onStart={handleStart}
-                    onPause={handlePause}
-                    onEnded={handleEnded}
-                />
-            </div>
-        </div>
-    </div>)
+        vapPlayer.current = new Vap().play(Object.assign({}, {
+            container: vapPlayer.current,
+            // 素材视频链接
+            src: demo,
+            // 素材配置json对象
+            config: config,
+            width: 900,
+            height: 400,
+            // 同素材生成工具中配置的保持一致
+            fps: 20,
+            // 是否循环
+            loop: true,
+            // 起始播放时间点
+            beginPoint: 0,
+            // 精准模式
+            accurate: true
+            // 播放起始时间点(秒)
+        }, {
+            // 融合信息（图片/文字）,同素材生成工具生成的配置文件中的srcTag所对应，比如[imgUser] => imgUser
+            imgUser: man,
+            imgAnchor: woman,
+            textUser: 'jiebin',
+            textAnchor: 'mengyin',
+            type: 2
+        }))
+        vapPlayer2.current = new Vap().play(Object.assign({}, {
+            container: vapPlayer2.current,
+            // 素材视频链接
+            src: demo2,
+            // 素材配置json对象
+            config: config2,
+            width: 900,
+            height: 400,
+            // 同素材生成工具中配置的保持一致
+            fps: 20,
+            // 是否循环
+            loop: true,
+            // 起始播放时间点
+            beginPoint: 0,
+            // 精准模式
+            accurate: true
+            // 播放起始时间点(秒)
+        }, {
+            // 融合信息（图片/文字）,同素材生成工具生成的配置文件中的srcTag所对应，比如[imgUser] => imgUser
+            textUser: 'jiebin',
+            textAnchor: 'mengyin',
+            type: 2
+        }))
+    }, []);
+    return (
+        <>
+            <div ref={vapPlayer}/>
+            <div ref={vapPlayer2}/>
+        </>
+    )
 }
 
 export default Test
