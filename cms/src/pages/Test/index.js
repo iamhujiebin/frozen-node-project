b26fe37def646810e1884af37a077f8187850fc0
iamhujiebin <458249864@qq.com>
2023-06-28 10:05:02 +0800

    fix:vap制造done

--- cms/src/pages/Test/index.js
+++ cms/src/pages/Test/index.js
@@ -1,8 +1,4 @@
 import {useEffect, useRef, useState} from "react";
-import {getToken} from "@/utils";
-import {createWebSocket} from "@/components/WebSocket";
-import {PubSub} from 'pubsub-js';
-import ReactPlayer from "react-player";
 import Vap from 'video-animation-player'
 import config from './demo.json'
 import demo from './demo.mp4'
@@ -15,6 +11,7 @@ import woman from "@/assets/woman.png"
 function Test() {
     let vapPlayer = useRef(null);
     let vapPlayer2 = useRef(null);
+    const [name,setName] = useState("皇马嗯")
 
     useEffect(() => {
         vapPlayer.current = new Vap().play(Object.assign({}, {
@@ -23,10 +20,10 @@ function Test() {
             src: demo,
             // 素材配置json对象
             config: config,
-            width: 900,
-            height: 400,
+            // width: 900,
+            // height: 400,
             // 同素材生成工具中配置的保持一致
-            fps: 20,
+            // fps: 20,
             // 是否循环
             loop: true,
             // 起始播放时间点
@@ -48,10 +45,10 @@ function Test() {
             src: demo2,
             // 素材配置json对象
             config: config2,
-            width: 900,
-            height: 400,
+            // width: 900,
+            // height: 400,
             // 同素材生成工具中配置的保持一致
-            fps: 20,
+            // fps: 20,
             // 是否循环
             loop: true,
             // 起始播放时间点
@@ -61,15 +58,14 @@ function Test() {
             // 播放起始时间点(秒)
         }, {
             // 融合信息（图片/文字）,同素材生成工具生成的配置文件中的srcTag所对应，比如[imgUser] => imgUser
-            textUser: 'jiebin',
-            textAnchor: 'mengyin',
-            type: 2
+            name: name,
         }))
-    }, []);
+    }, [name]);
     return (
         <>
             <div ref={vapPlayer}/>
             <div ref={vapPlayer2}/>
+            <p>{name}</p>
         </>
     )
 }
