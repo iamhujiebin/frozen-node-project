37532c3496256a8d7af6c42463e70eab89a0e9a7
hujiebin <iamhujiebin@users.noreply.github.com>
2023-06-28 17:43:14 +0800

    feat删除无用的

--- cms/src/utils/history.js
+++
@@ -1,7 +0,0 @@
-// import {createBrowserHistory, createHashHistory} from "history"
-import {createHashHistory} from "history"
-import {unstable_HistoryRouter as HistoryRouter} from "react-router-dom"
-
-const history = createHashHistory() // 前端路由用hash,后端路由用Browser
-
-export {HistoryRouter, history}
--- cms/src/utils/index.js
+++
@@ -1,5 +0,0 @@
-import {http} from './http'
-import {getToken, setToken, clearToken, getMobile, setMobile, getCode, setCode} from './token'
-import {HistoryRouter, history} from './history'
-
-export {http, getToken, setToken, clearToken, getMobile, setMobile, setCode, getCode, HistoryRouter, history}
